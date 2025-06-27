9adf49608cdb1a2297ac13091d096c50162b9b05
Erwin Jimenez <erwinjv98@gmail.com>
2025-06-26 20:14:16 -0400

    Improve design of alarm layout, add button to stop alarm, capability of delete alarms

--- src/components/AlarmClock/AlarmClock.tsx
+++ src/components/AlarmClock/AlarmClock.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { TimeContext } from "../../context/time-context";
+import { padZero } from "../../helpers/pad-zero";
 
 export default React.memo(function AlarmClock() {
   const time = useContext(TimeContext) || new Date();
@@ -18,10 +19,10 @@ export default React.memo(function AlarmClock() {
   }
 
   return (
-    <div className="bg-black p-10 inline-block">
-      <div className="bg-white p-4 ">
-        <div className="bg-[#748079] p-1 flex justify-center">
-          <span className="font-ds-digital text-4xl sm:text-5xl md:text-7xl lg:text-9xl ">
+    <div className="bg-black p-10 flex ">
+      <div className="bg-white p-4  flex">
+        <div className="bg-[#748079] w-[172px] md:w-[400px] flex justify-center items-center p-2">
+          <span className="font-ds-digital  text-4xl  md:text-7xl text-center  ">
             {formatTime()}
           </span>
         </div>
@@ -29,7 +30,3 @@ export default React.memo(function AlarmClock() {
     </div>
   );
 });
-
-function padZero(num: number) {
-  return (num < 10 ? "0" : "") + num;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { GlobalStyles, StyledEngineProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>
 );
