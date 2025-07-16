9461e34061de149891a90bf5339e4a63b240d911
Erwin Jimenez <erwinjv98@gmail.com>
2025-07-15 22:54:45 -0400

    Refactor clock and improve selectSongs

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,120 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 
-import { AlarmsContext } from "./context/alarms";
-import { TimeContext } from "./context/time-context";
 import AlarmClock from "./components/AlarmClock/AlarmClock";
 
-import type { Alarms } from "./types/alarm";
-
-import { activateAlarm } from "./helpers/activate-alarm";
 import { useSound } from "./hooks/activate-alarm";
 
-import sound from "./assets/TimeToMakeHistoryShihokoHirata.mp3";
-
+import timeToMakeHistory from "./assets/songs/time-to-make-history/TimeToMakeHistoryShihokoHirata.mp3";
 import p4GImage from "./assets/songs/time-to-make-history/p4GT.jpg";
 
+import sirAlonneSong from "./assets/songs/sir-alonne/sir-alonne.mp3";
+import sirAlonneCover from "./assets/songs/sir-alonne/sir-alonne .jpg";
+
+import spanishGuitarSong from "./assets/songs/spanish-guitar/spanish-guitar.mp3";
+import spanishGuitarCover from "./assets/songs/spanish-guitar/spanish-guitar.jpg";
+
 import SelectSong from "./components/SelectSong";
+import type { Song } from "./types/song";
+import { Button } from "@mui/material";
+
+const songs: Song[] = [
+  {
+    coverPath: p4GImage,
+    soundPath: timeToMakeHistory,
+    title: "Time to make history",
+  },
+  {
+    coverPath: sirAlonneCover,
+    soundPath: sirAlonneSong,
+    title: "Sir Alonne",
+  },
+  {
+    coverPath: spanishGuitarCover,
+    soundPath: spanishGuitarSong,
+    title: "Spanish Guitar",
+  },
+];
 
 export default React.memo(function App() {
-  const [time, setTime] = useState(new Date());
-  const [alarms, setAlarms] = useState<Alarms>({});
+  const [currentSong, setCurrentSong] = useState<number>(0);
+  const [isSettled, setIsSettled] = useState<boolean>(false);
 
-  const { isPlaying, play } = useSound(sound);
+  const { isPlaying, play, stop } = useSound(songs[currentSong].soundPath);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const date = new Date().toLocaleString().split(",")[1].trimStart();
-
-      if (alarms[date]) {
-        activateAlarm();
-        play();
-      }
-
-      if (!isPlaying) {
-        setTime(new Date());
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [alarms, isPlaying, play]);
-
-  useEffect(() => {
-    if (Object.keys(alarms).length) {
-      window.localStorage.setItem("ALARMS", JSON.stringify(alarms));
+  const toggleSong = useCallback(() => {
+    if (isPlaying) {
+      stop();
+    } else {
+      play();
     }
-  }, [alarms]);
+  }, [isPlaying, play, stop]);
 
-  useEffect(() => {
-    const data = window.localStorage.getItem("ALARMS") || "{}";
+  const selectNextSong = useCallback(() => {
+    if (isSettled) return;
+    if (isPlaying) return;
 
-    setAlarms(JSON.parse(data));
+    if (currentSong + 1 > songs.length - 1) {
+      setCurrentSong(0);
+      return;
+    }
+
+    setCurrentSong((current) => current + 1);
+  }, [currentSong, isPlaying, isSettled]);
+
+  const selectPreviousSong = useCallback(() => {
+    if (isSettled) return;
+    if (isPlaying) return;
+
+    if (currentSong - 1 < 0) {
+      setCurrentSong(songs.length - 1);
+      return;
+    }
+
+    setCurrentSong((current) => current - 1);
+  }, [currentSong, isPlaying, isSettled]);
+
+  const toggleSettled = useCallback(() => {
+    setIsSettled((current) => !current);
   }, []);
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-[url(./assets/clock-square-svgrepo-com.svg)] bg-size-[10px]">
-      <TimeContext value={time}>
-        <AlarmsContext value={alarms}>
-          <div className="flex flex-col md:flex-row">
-            <AlarmClock />
-            <SelectSong
-              coverPath={p4GImage}
-              selectNextSong={() => {}}
-              selectPreviousSong={() => {}}
-              title="Time to make history"
-            />
-          </div>
-        </AlarmsContext>
-      </TimeContext>
+    <div className="w-screen h-screen flex justify-center items-center bg-[url(./assets/backgrounds/dawn.jpg)] bg-cover">
+      <div className="w-full flex flex-col items-center md:flex-row md:justify-around">
+        <div className="flex flex-col items-center my-4 md:my-0 ">
+          <AlarmClock play={play} isSettled={isSettled} />
+          {isSettled ? (
+            <span className="text-xl text-white my-1">Alarm is set!</span>
+          ) : (
+            <div className="h-[28px]"></div>
+          )}
+          <Button
+            className="uppercase font-bold text-2xl "
+            onClick={toggleSettled}
+            color="error"
+            variant="contained"
+          >
+            {isSettled ? "Cancel" : "Set Alarm"}
+          </Button>
+        </div>
+
+        <div className="flex flex-col items-center my-4 md:my-0 ">
+          <h4 className="text-xl font-bold">Select Song</h4>
+          <SelectSong
+            coverPath={songs[currentSong].coverPath}
+            selectNextSong={selectNextSong}
+            selectPreviousSong={selectPreviousSong}
+            title={songs[currentSong].title}
+          />
+          <Button
+            className="uppercase font-bold text-2xl "
+            onClick={toggleSong}
+            color="error"
+            variant="contained"
+          >
+            {isPlaying ? "stop" : "play"}
+          </Button>
+        </div>
+      </div>
     </div>
   );
 });
--- src/components/AlarmClock/AlarmClock.tsx
+++ src/components/AlarmClock/AlarmClock.tsx
@@ -1,84 +1,110 @@
-import React, { useCallback, useContext, useState } from "react";
-import { TimeContext } from "../../context/time-context";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
 import { padZero } from "../../helpers/pad-zero";
 import Pad from "../Pad";
 import { IoAdd, IoRemove } from "react-icons/io5";
 import { RadioGroup, FormControlLabel, Radio } from "@mui/material";
 
-export default React.memo(function AlarmClock() {
-  const time = useContext(TimeContext) || new Date();
+interface AlarmClockProps {
+  play: () => void;
+  isSettled: boolean;
+}
 
+export default React.memo<AlarmClockProps>(function AlarmClock({
+  play,
+  isSettled,
+}) {
   const [hour, setHour] = useState<string>("00");
   const [minutes, setMinutes] = useState<string>("00");
   const [meridiem, setMeridiem] = useState<"AM" | "PM">("AM");
+  const currentTimeRef = useRef<HTMLSpanElement>(
+    document.createElement("span")
+  );
 
   const minHour = useCallback(() => {
+    if (isSettled) return;
     const numHour = parseInt(hour);
 
     const res = numHour - 1;
     if (res < 1) return;
     setHour(padZero(res));
-  }, [hour]);
+  }, [hour, isSettled]);
 
   const maxHour = useCallback(() => {
+    if (isSettled) return;
     const numHour = parseInt(hour);
     const res = numHour + 1;
     if (res > 12) return;
     setHour(padZero(res));
-  }, [hour]);
+  }, [hour, isSettled]);
 
   const minMinute = useCallback(() => {
+    if (isSettled) return;
     const numMinutes = parseInt(minutes);
 
     const res = numMinutes - 1;
     if (res < 1) return;
     setMinutes(padZero(res));
-  }, [minutes]);
+  }, [isSettled, minutes]);
 
   const maxMinute = useCallback(() => {
+    if (isSettled) return;
     const numMinutes = parseInt(minutes);
     const res = numMinutes + 1;
     if (res > 59) return;
     setMinutes(padZero(res));
-  }, [minutes]);
+  }, [isSettled, minutes]);
 
   const setMeridiemToAM = useCallback(() => {
+    if (isSettled) return;
     setMeridiem("AM");
-  }, []);
+  }, [isSettled]);
 
   const setMeridiemToPM = useCallback(() => {
+    if (isSettled) return;
     setMeridiem("PM");
-  }, []);
+  }, [isSettled]);
 
-  function formatTime() {
-    let hours = time?.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    const meridiem = hours >= 12 ? "PM" : "AM";
+  // function formatTime() {
+  //   let hours = time?.getHours();
+  //   const minutes = time.getMinutes();
+  //   const seconds = time.getSeconds();
+  //   const meridiem = hours >= 12 ? "PM" : "AM";
 
-    hours = hours % 12 || 12;
+  //   hours = hours % 12 || 12;
 
-    return `${padZero(
-      hours
-    )}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
-  }
+  //   return `${padZero(
+  //     hours
+  //   )}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+  // }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const date = new Date().toLocaleString().split(",")[1].trimStart();
+      currentTimeRef.current.innerHTML = `Current time: ${date}`;
+      if (date === `${hour}:${minutes}:00 ${meridiem}`) {
+        play();
+      }
+      console.log({ date, alarm: `${hour}:${minutes}:00 ${meridiem}` });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [hour, meridiem, minutes, play]);
 
   return (
     <div className="bg-black p-6 flex flex-col rounded-2xl ">
       <div className="text-white flex justify-between ">
         <Pad amt={20} row />{" "}
-        <span className="text-xs font-bold p-1">
-          Current time is: {formatTime()}
+        <span ref={currentTimeRef} className="text-xs font-bold p-1">
+          {/* Current time is: {formatTime()} */}
         </span>
       </div>
       <div className="bg-[#748079]  flex justify-between items-center p-2">
-        <span className="font-ds-digital  text-4xl  md:text-7xl text-center  ">
-          {hour}
-        </span>
-        <span className="font-ds-digital  text-4xl  md:text-7xl text-center  ">
-          :
-        </span>
-        <span className="font-ds-digital  text-4xl  md:text-7xl text-center  ">
+        <span className="font-ds-digital  text-7xl text-center  ">{hour}</span>
+        <span className="font-ds-digital  text-7xl text-center  ">:</span>
+        <span className="font-ds-digital  text-7xl text-center  ">
           {minutes}
         </span>
       </div>
